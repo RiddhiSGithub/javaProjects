cdcc78e7aa4dd6b67a582224c81ee27df2cb686d
Riddhiben Makwana <riddhimakwana77@gmail.com>
2023-11-04 15:34:05 -0400

    Arrays practice

--- hi.java
+++ hi.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 public class hi {
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
+        // creating 100 arry string
+        String[] array = new String[5];
+
+        //scan user name 
+        Scanner scanner = new Scanner(System.in);
+
+        //create a loop to add
+        for(int i = 0;i<=array.length;i++){
+            if(i==array.length){
+                System.out.println("this block is full.");
+                break;
+            }
+            System.out.println("Enter your username:");
+            String username = scanner.nextLine();
+            array[i] = username;
+        }
+        //print the result
+        for (int i = 0;i<array.length;i++) {
+            System.out.println("User-"+(i+1)+" Name is "+array[i]+".");
+            }
+        //close the scanner
+        scanner.close();
     }
 }
