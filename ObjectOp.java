e021b4f647ea6bc6461dcdbd35a85db5d0cb61d5
Riddhiben Makwana <riddhimakwana77@gmail.com>
2024-01-15 23:41:55 -0500

    study

---
+++ ObjectOp.java
@@ -0,0 +1,14 @@
+class riddhi{
+    String name;
+    public void riddhi(String name){
+        System.out.println(name);
+    }
+}
+
+public class ObjectOp {
+    public static void main(String arg[]){
+        riddhi r = new riddhi();
+        String name = "Riddhi";
+        r.riddhi(name);
+    }
+}
